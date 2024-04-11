cf42261daaaf703231dbe0d578ed83c6a51dc23d
iwanharyatno <iwnharry61@gmail.com>
2024-04-11 17:59:11 +0700

    calculator: create post-test only solution

--- kalkulator/script.js
+++ kalkulator/script.js
@@ -23,6 +23,36 @@ document
       }
       return;
     }
+
+    if (event.target.classList.contains("action-percent")) {
+      layar.innerText = Number(layar.innerText) / 100;
+      return;
+    }
+
+    if (event.target.classList.contains("action-sin")) {
+      const valueRad = Number(layar.innerText) * Math.PI / 180;
+      console.log(valueRad);
+      layar.innerText = bulatBelakangKoma(Math.sin(valueRad), 7);
+      return;
+    }
+
+    if (event.target.classList.contains("action-cos")) {
+      const valueRad = Number(layar.innerText) * Math.PI / 180;
+      layar.innerText = bulatBelakangKoma(Math.cos(valueRad), 7);
+      return;
+    }
+
+    if (event.target.classList.contains("action-tan")) {
+      const valueRad = Number(layar.innerText) * Math.PI / 180;
+      layar.innerText = bulatBelakangKoma(Math.tan(valueRad), 7);
+      return;
+    }
     
     layar.innerText += event.target.value;
   });
+
+function bulatBelakangKoma(angka, belakangKoma) {
+  const puluhan = Math.pow(10, belakangKoma);
+  console.log(puluhan);
+  return Math.round(angka * puluhan) / puluhan;
+}
